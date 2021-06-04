7fb2a280d8a292ef71105494db946c6565c986a4
sivabharathy <gotocva@gmail.com>
2021-06-04 23:29:58 +0530

    session access given for routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,10 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 use Sparkouttech\UserAuth\app\Http\Controllers\UserController;
-Route::get('/package', function () {
-    echo "Package working fine";
+
+Route::group(['middleware' => 'web'], function() {
+    //All the routes that belongs to the group goes here
+    Route::get('/auth/user/login', [UserController::class, 'login'])->name('userAuth.login.page');
+    Route::post('/auth/user/login', [UserController::class, 'doLogin'])->name('userAuth.login');
+    Route::get('/auth/user/register', [UserController::class, 'register'])->name('userAuth.register.page');
+    Route::post('/auth/user/register', [UserController::class, 'doRegister'])->name('userAuth.register');
 });
-Route::get('/auth/user/login', [UserController::class, 'login'])->name('userAuth.login.page');
-Route::post('/auth/user/login', [UserController::class, 'doLogin'])->name('userAuth.login');
-Route::get('/auth/user/register', [UserController::class, 'register'])->name('userAuth.register.page');
-Route::post('/auth/user/register', [UserController::class, 'doRegister'])->name('userAuth.register');
+
