a981fc8490a4b92c043036aa94ced098fc253887
sivabharathy <gotocva@gmail.com>
2021-06-06 14:08:52 +0530

    comment name update

--- src/config/user-auth.php
+++ src/config/user-auth.php
@@ -5,20 +5,16 @@
  */
 return [
 
-    /**
-     * Enable / Disable referral module
-     */
+    // Enable / Disable referral module
     "referral" => true,
     "referral_code_length" => 10,
 
-    /**
-     *
-     */
+    // crypto configuration files
     "crypto" => [
         "ciphering" => "AES-128-CTR",
         "options" => 0,
         "key" => "SparkoutUserAuth",
         "iv" => "1234567891011121"
-    ]
+    ],
 
 ];
